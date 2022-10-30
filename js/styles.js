2c84950414233c639092b4ac5a831df757744320
Auston Moos <AustonHMoos2004@gmail.com>
2022-10-29 20:39:16 -0700

    add answer/querry selector

---
+++ js/styles.js
@@ -0,0 +1,14 @@
+// UI Logic
+
+function useArray() {
+
+  const answer1 = document.querySelector("input[name='question1']:checked");
+  const answer2 = document.querySelector("input[name='question2']:checked");
+  const answer3 = document.querySelector("input[name='question3']:checked");
+  const answer4 = document.querySelector("input[name='question4']:checked");
+  const answer5 = document.querySelector("input[name='question5']:checked");
+
+  answers = [answer1,answer2,answer3,answer4,answer5,answer6,answer7];
+
+}
+
